b1f9f0d2f3004aef725b15fbd5fb708f45825f2e
ErlanBelekov <belekoverlanhehe@gmail.com>
2020-05-28 22:24:15 +0600

    add constants

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const API: String = "https://api.openweathermap.org/data/2.5/";
+// this is used to retrieve images and other static content
+export const StaticContentAPI: String = "http://openweathermap.org";
+
+export class BackgroundColors {
+  static snow = "#00ffff"
+  static warm = "#fff700"
+  static hot = "#ff8c00"
+  static cool = "#2980b9"
+  static default = "#fff"
+}
